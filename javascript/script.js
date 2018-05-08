6853a50a612bfdf953dc8a6122f01dd74677da4c
Maya Miller <mxm2038@miami.edu>
2018-05-07 23:31:00 -0400

    final

---
+++ javascript/script.js
@@ -0,0 +1,30 @@
+var index= 1;
+showSlides(index);
+
+function plusSlides(n){
+  showSlides(index +=n);
+}
+
+function currentSlide(n){
+  showSlides(index = n);
+}
+
+function showSlides(n){
+  var i;
+  var slides=
+  document.getElementsByClassName("slides");
+
+  if(n>slides.length) {
+    index =1
+  }
+
+  if(n<1){
+    index=slides.length
+  }
+
+  for(i=0; i<slides.length; i++){
+    slides[i].style.display="none";
+  }
+
+slides[index-1].style.display="block";
+}
